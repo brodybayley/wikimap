c9fad72d544fd28cda6b29e92885c508418d4ea9
Brody Bayley <brodybayley@gmail.com>
2021-03-08 23:13:45 +0000

    Add route for adding a point

--- routes/points.js
+++ routes/points.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { getMapPoints, getPoint } = require('../db/product-queries');
 
-//GET /maps/:map_id/points
+
+//Get all points on a map /maps/:map_id/points
 router.get("/points", (req, res) => {
   getMapPoints(mapID)
     .then((points) => {
@@ -28,9 +29,8 @@ const getMapPoints = mapID => {
     .then(res => res.rows);
 };
 
-module.exports = router;
 
-//GET /maps/:map_id/points/:point_id
+//Get a specific point /maps/:map_id/points/:point_id
 router.get("/points/:point_id", (req, res) => {
   getPoint(pointID)
     .then((point) => {
@@ -55,7 +55,60 @@ const getPoint = pointID => {
     .then(res => res.rows[0]);
 };
 
+
+// //Edit a point /maps/:map_id/points/:point_id
+// router.post("/points/:point_id", (req, res) => {
+//   getPoint(pointID)
+//     .then((point) => {
+//       res.json(point);
+//     })
+//     .catch(err => {
+//       res
+//         .status(500)
+//         .json({ error: err.message });
+//     });
+// });
+
+// const editPoint = pointID => {
+//   const queryStr = `
+//   UPDATE points
+//   SET
+//   WHERE id = $2
+//   `;
+
+//   return db
+//     .query(queryStr, [pointID])
+//     .then(res => res.rows[0]);
+// };
+
+
+// Add a point to a map /maps/:map_id/points
+router.post("/points", (req, res) => {
+  addPoint(pointID)
+    .then((point) => {
+      res.json(point);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+const addPoint = (point) => {
+  const queryStr = `
+  INSERT INTO points (map_id, user_id, title, description, longitude, latitude, image_url)
+  VALUES ($1, $2, $3, $4, $5, $6, $7)
+  RETURNING *
+  `;
+
+  return db
+    .query(queryStr, [point.map_id, point.user_id, point.title, point.description, point.longitude, point.latitude, point.image_url])
+    .then(res => res.rows[0]);
+};
+
+exports.addPoint = addPoint;
+
 // ### Points
-// * Edit    => POST   =>  '/maps/:map_id/points/:point_id'
-// * Add     => POST   =>  '/maps/:map_id/points'
+
 // * Delete  => DELETE =>  '/maps/:map_id/points/:point_id'
