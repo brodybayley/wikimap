509dab90f72ae380dd1a8469feeeb61d0585caaf
Sandra To <sandra.to.e@gmail.com>
2021-03-08 21:38:40 +0000

    Add query functions modifying maps table

--- db/queries/map-queries.js
+++ db/queries/map-queries.js
@@ -23,8 +23,35 @@ const getMapsById = id => {
     .then(res => res.rows[0]);
 };
 
+const addMap = map => {
+  const queryStr = `
+    INSERT INTO maps (user_id, title, description)
+    VALUES ($1, $2, $3)
+    RETURNING *
+  `;
+
+  const values = [map.user_id, map.title, map.description];
+
+  return db
+    .query(queryStr, values)
+    .then(res => res.rows[0]);
+};
+
+const deleteMap = mapId => {
+  const queryStr = `
+    DELETE FROM maps
+    WHERE id = $1
+    RETURNING *
+  `;
+
+  return db
+    .query(queryStr, [mapId])
+    .then(res => res.rows);
+};
 
 module.exports = {
   getMaps,
   getMapsById,
+  addMap,
+  deleteMap
 };
