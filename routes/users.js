f17e29dff680a10eedba954671b3a0297bfacbbe
Sandra To <sandra.to.e@gmail.com>
2021-03-09 07:38:50 +0000

    Copy and paste user-queries from failed merge

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { registerUsers, getFavouriteMaps, getMyMaps, getUsers } = require('./db/userQueries.js')
+const { registerUsers, getFavouriteMaps, getMyMaps, getUsers } = require('../db/user-queries.js')
 
 // TO DO: ejs files
 router.get('/register', (req, res) => {
