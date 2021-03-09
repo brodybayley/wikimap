a9a1abbcd85a7d320090a5a2c57575fa09b23e84
Sandra To <sandra.to.e@gmail.com>
2021-03-09 19:39:52 +0000

    Move database codes to same file and establish db connection

--- server.js
+++ server.js
@@ -12,12 +12,6 @@ const morgan         = require('morgan');
 const cookieSession  = require('cookie-session');
 const methodOverride = require('method-override');
 
-// PG database client/connection setup
-const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
-const db = new Pool(dbParams);
-db.connect();
-
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -39,19 +33,14 @@ app.use(cookieSession({
 app.use(methodOverride('_method'));
 
 // Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const mapsRoutes = require("./routes/maps");
+const usersRoutes  = require("./routes/users");
+const mapsRoutes   = require("./routes/maps");
 const pointsRoutes = require("./routes/points");
-const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes);
 app.use("/api/maps", mapsRoutes);
 app.use("/api/points", pointsRoutes);
-app.use("/api/widgets", widgetsRoutes(db));
-// Note: mount other resources here, using the same pattern above
 
 
 // Home page
