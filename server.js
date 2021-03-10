840fd3975d92039a6813faed0cb5b83a62823e26
Sandra To <sandra.to.e@gmail.com>
2021-03-10 08:36:40 +0000

    Display different nav cta to user vs guest

--- server.js
+++ server.js
@@ -52,7 +52,9 @@ app.use("/api/maps", pointsRoutes);
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  const userId = req.session.userId;
+  const templateVars = {user: userId};
+  res.render("index", templateVars);
 });
 
 app.listen(PORT, () => {
