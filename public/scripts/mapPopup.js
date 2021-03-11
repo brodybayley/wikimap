137a53db2b328cfc82aa2e86676bf5c2ca4f6990
Sandra To <sandra.to.e@gmail.com>
2021-03-11 01:59:30 +0000

    Working codes can serialize data

--- public/scripts/mapPopup.js
+++ public/scripts/mapPopup.js
@@ -10,4 +10,32 @@ $(() => {
   $('#close-button').on('click', () => {
     $('.form-popup').toggle();
   });
+
+  $('.map-form').on('submit', event => {
+    event.preventDefault();
+    const title = $('#input-map-title').val();
+    const description = $('#input-map-description').val();
+    console.log(title);
+    console.log(description);
+    const serializedForm = $('form').serialize();
+    console.log('leform', serializedForm);
+    $.ajax.post({url: '/api/map'}, {data: serializedForm});
+    //this post needs addMaps
+
+
+// const addMap = map => {
+//   const queryStr = `
+//     INSERT INTO maps (user_id, title, description)
+//     VALUES ($1, $2, $3)
+//     RETURNING *
+//   `;
+
+//   const values = [map.user_id, map.title, map.description];
+
+//   return db
+//     .query(queryStr, values)
+//     .then(res => res.rows);
+// };
+    //$.ajax({ url: '/tweets', method: 'POST', data: $(this).serialize() })
+  });
 });
