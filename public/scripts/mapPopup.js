f24b992b4f24bb20e4f178054c68fe601c5718b6
Sandra To <sandra.to.e@gmail.com>
2021-03-11 03:25:37 +0000

    Refactor codes and add comments in prep for merge

--- public/scripts/mapPopup.js
+++ public/scripts/mapPopup.js
@@ -1,29 +1,28 @@
 /* global $ */
 
 $(() => {
+  // Map form pop up appears
   $('.create-map-cta').on('click', event => {
     event.preventDefault();
 
     $('.form-popup').toggle();
   });
 
+  // Map form pop up disappears
   $('#close-button').on('click', () => {
     $('.form-popup').toggle();
   });
 
+  // Create new map to database
   $('.map-form').on('submit', event => {
     event.preventDefault();
 
-    $.ajax({
-      method: 'POST',
+    $.post({
       url: '/api/maps',
       data: $('form').serialize(),
-      success: function() {
-        window.location.replace('http://localhost:8080/maps');
-      }
     })
       .then(res => {
-        console.log(res);
+        window.location.replace('http://localhost:8080/maps');
       })
       .catch(err => console.log(err));
   });
