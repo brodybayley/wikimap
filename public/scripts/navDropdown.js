2283da4f9d31b29dc8ff829554de8f5c3447ab2f
Sandra To <sandra.to.e@gmail.com>
2021-03-11 21:22:32 +0000

    Hide dropdown when clicking anywhere outside of nav container

--- public/scripts/navDropdown.js
+++ public/scripts/navDropdown.js
@@ -1,4 +1,5 @@
 /* global $ */
+/* global document */
 /* global window */
 
 $(() => {
@@ -6,18 +7,20 @@ $(() => {
     $('.nav-dropdown-content').toggle();
   });
 
+  $(document).click(event => {
+    if ($(event.target).closest('.dropdown').length === 0) {
+      $('.nav-dropdown-content').hide();
+    }
+  });
+
   $('#nav-logout').on('click', event => {
     event.preventDefault();
 
     $.post('/logout')
-    .then(() => {
-      window.location.replace('http://localhost:8080/');
-    })
-    .catch(err => console.log(err));
+      .then(() => {
+        window.location.replace('http://localhost:8080/');
+      })
+      .catch(err => console.log(err));
   });
-
-  // window.onclick = () => {
-  //   $('.nav-dropdown-content').toggle();
-  // };
 
 });
