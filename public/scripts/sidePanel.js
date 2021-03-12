2860086ab7b1920323d1b400febaad4364b05369
Sandra To <sandra.to.e@gmail.com>
2021-03-12 09:44:14 +0000

    Render maps from hardcoded array data

--- public/scripts/sidePanel.js
+++ public/scripts/sidePanel.js
@@ -31,31 +31,44 @@ $(() => {
 
   // Load user created maps
   const data =
+  [
     {
       "id": 1,
       "user_id": 1,
       "title": "Construction Sites",
       "description": "Current constructions going on around Vancouver. Be careful of manholes!",
       "created_on": "2021-03-12T04:11:30.291Z"
-    };
+    },
+    {
+      "id": 2,
+      "user_id": 3,
+      "title": "Food is Life",
+      "description": "The title says it all. Food 4eva!!!",
+      "created_on": "2021-03-12T04:11:30.291Z"
+    },
+    {
+      "id": 3,
+      "user_id": 4,
+      "title": "Bird Watching Spots",
+      "description": "Grab a chair and sit at these spots to look at birds.",
+      "created_on": "2021-03-12T04:11:30.291Z"
+    }
+  ];
 
   const createMapItem = map => {
     let $map = $('<p>').text(map.title);
     return $map;
   };
 
-  $('#my-maps').append(createMapItem(data));
-  // console.log(createMapItem(data));
+  const renderMaps = maps => {
+    $('#my-maps').empty();
 
-  // const renderMaps = maps => {
-  //   $('#my-maps').empty();
+    for (let map of maps) {
+      $('#my-maps').append(createMapItem(map));
+    }
+  };
 
-  //   for (let map of maps) {
-  //     $('#my-maps').append(createMapItem(map));
-  //   }
-  // };
-
-  // renderMaps();
+  renderMaps(data);
 
   // const loadMaps = () => {
   //   const userId = req.session.userId;
