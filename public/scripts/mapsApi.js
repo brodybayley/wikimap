5152328b4b8bb7d54254575a53b77129415a15bf
Ray Yiu <rayyiu66@gmail.com>
2021-03-12 03:29:01 -0800

    preparing to merge

--- public/scripts/mapsApi.js
+++ public/scripts/mapsApi.js
@@ -14,10 +14,10 @@
 // });
 
 function initMap() {
-    const mapId = 2;
+    const mapId = 1;
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
-        center: { lat: -34.397, lng: 150.644 },
+        center: { lat: 49.2827, lng: -123.1207 },
     });
     const addMarker = function (coords) {
         console.log('coords', coords);
