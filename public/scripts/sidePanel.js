e88d3b5dc786b519cc695613bca3533db8acb328
Sandra To <sandra.to.e@gmail.com>
2021-03-12 09:32:45 +0000

    Render points from array of hardcoded data

--- public/scripts/sidePanel.js
+++ public/scripts/sidePanel.js
@@ -1,16 +1,28 @@
 /* global $ */
 
 $(() => {
-  const data = {
-    "id": 3,
-    "map_id": 1,
-    "user_id": 1,
-    "title": "Manhole Discovered",
-    "description": "description",
-    "longitude": 49.266239,
-    "latitude": -123.162111,
-    "image_url": "https://images.unsplash.com/photo-1577896021507-78957e4b77d2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Y29uc3RydWN0aW9uc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"
-  };
+  const data = [
+    {
+      "id": 3,
+      "map_id": 1,
+      "user_id": 1,
+      "title": "Manhole Discovered",
+      "description": "description",
+      "longitude": 49.266239,
+      "latitude": -123.162111,
+      "image_url": "https://images.unsplash.com/photo-1577896021507-78957e4b77d2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Y29uc3RydWN0aW9uc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"
+    },
+    {
+      "id": 4,
+      "map_id": 1,
+      "user_id": 3,
+      "title": "New Building at CityHall",
+      "description": "description",
+      "longitude": 49.26333,
+      "latitude": -123.11514,
+      "image_url": "https://images.unsplash.com/photo-1575371495064-0df0980c3d4c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Y29uc3RydWN0aW9uc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"
+    }
+  ];
 
   const createPointItem = point => {
     let $point = $('<p>')
@@ -20,8 +32,16 @@ $(() => {
   };
 
 
-  console.log(createPointItem(data));
-  $('#pin-list').append(createPointItem(data));
+  const renderPoints = points => {
+    $('#pin-list').empty();
+
+    for (let point of points) {
+      $('#pin-list').append(createPointItem(point));
+    }
+  };
+
+  renderPoints(data);
+
 
 
   // const data =
