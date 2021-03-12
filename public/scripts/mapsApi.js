793fa5797d82f55329caa672f9606d94abe8c6e6
Ray Yiu <rayyiu66@gmail.com>
2021-03-12 09:41:19 -0800

    trying to make points reappear

--- public/scripts/mapsApi.js
+++ public/scripts/mapsApi.js
@@ -14,17 +14,18 @@
 // });
 
 function initMap() {
-    const mapId = 1;
+    const mapId = 2;
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
         center: { lat: 49.2827, lng: -123.1207 },
     });
     const addMarker = function (coords) {
         console.log('coords', coords);
-        let marker = new google.maps.Marker({
+        const marker = new google.maps.Marker({
             position: new google.maps.LatLng(coords),
             map: map
         });
+        console.log('marker', marker);
     };
     $.get({
         url: `/api/maps/${mapId}/points`,
@@ -64,7 +65,7 @@ const addPoint = ({ latitude, longitude }, data) => {
     //     point.image_url
     //   ];
     $.post({
-        url: `/api/points/${mapId}/points`,
+        url: `/api/maps/${mapId}/points`,
         // data: $('#point-form').serialize()
         data: { user_id: 1, map_id: mapId, description: description, latitude: latitude, title: title, image_url: image_url, longitude: longitude }
     })
