bd1c91ceb1e63bb275a03788aa79b864de5c5ceb
Sandra To <sandra.to.e@gmail.com>
2021-03-12 11:52:54 +0000

    Clear lint errors

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,22 +1,25 @@
+/* global $ */
+/* global document */
+/* global window */
+
 $(() => {
 
-  const tabs = document.querySelectorAll('[data-tab-target]')
-  console.log('tabs:', tabs)
-  const tabContents = document.querySelectorAll('[data-tab-content]')
+  const tabs = document.querySelectorAll('[data-tab-target]');
+  const tabContents = document.querySelectorAll('[data-tab-content]');
 
   tabs.forEach(tab => {
     tab.addEventListener('click', () => {
-      const target = document.querySelector(tab.dataset.tabTarget)
+      const target = document.querySelector(tab.dataset.tabTarget);
       tabContents.forEach(tabContent => {
-        tabContent.classList.remove('active')
-      })
+        tabContent.classList.remove('active');
+      });
       tabs.forEach(tab => {
-        tab.classList.remove('active')
-      })
-      tab.classList.add('active')
-      target.classList.add('active')
-    })
-  })
+        tab.classList.remove('active');
+      });
+      tab.classList.add('active');
+      target.classList.add('active');
+    });
+  });
 
   //JS for kebab menu
   const kebab = document.querySelector('.kebab'),
@@ -24,11 +27,11 @@ $(() => {
     cross = document.querySelector('.cross'),
     dropdown = document.querySelector('.kebab-dropdown');
 
-  kebab.addEventListener('click', function () {
+  kebab.addEventListener('click', () => {
     middle.classList.toggle('active');
     cross.classList.toggle('active');
     dropdown.classList.toggle('active');
-  })
+  });
 
   // Controls back arrow
   function goBack() {
@@ -39,14 +42,14 @@ $(() => {
     event.preventDefault();
 
     $('#custom-point-feed').toggle();
-    $('#custom-map-feed').toggle()
-  })
+    $('#custom-map-feed').toggle();
+  });
 
   $('.kebab-container').on('click', event => {
     event.preventDefault();
 
     $('.kebab-dropdown').toggle();
-  })
+  });
 
 });
 
